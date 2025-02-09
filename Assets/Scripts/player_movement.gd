0cc4e36521eac9990863f91dd5d4ff66e3fa99f4
discoozin <d1sc0nn3ctzz@gmail.com>
2025-02-09 20:16:05 -0300

    Initial commit

---
+++ Assets/Scripts/player_movement.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var movement_speed: float=500
+var character_direction: Vector2
+
+func _physics_process(delta):
+	character_direction.x = Input.get_axis("move_left", "move_right")
+	character_direction.y = Input.get_axis("move_up", "move_down")
+	
+	#flip
+	if character_direction.x > 0: %sprite.flip_h = false
+	#elif character_direction.x <0: %sprite.filp_h = true
+	
+	if character_direction:
+		velocity = character_direction * movement_speed
+		#<adicionar sprite animation walking to idle 
+	else:	
+		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+	
+	move_and_slide()
