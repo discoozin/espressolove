88cf09d8e54d6b589bef3b26bbd7bab888ae2bca
NamoeL19 <manumachadog@gmail.com>
2025-02-11 09:44:36 -0300

    coloquei a cena do gerente
    fiz o script do gerente e para interagir com ele, não fiz mais nada ainda. precisa polir umas coisas

---
+++ Assets/Scripts/gerente.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+#exporta a variavel dialogo, sendo possivel alterar fora do codigo
+@export var dialogo: String
+
+#não precisa de explicação, é só a variavel pra ver se da pra interagir ou não BURRO BURRO BURRO
+var interativo = false
+
+#SEGUINTE, ISSO AQUI VERIFICA SE TODOS OS CORPOS QUE ESTÃO NO GRUPO "PLAYER" ENTRAREM NA AREA DA COLISÃO "INTERAÇÃO1"
+#SE ENTRAR, QUER DIZER Q O NPC É INTERATIVO.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		interativo = true
+
+#TODA VEZ QUE O PLAYER SAIR DA AREA DE "INTERAÇÃO1", O INTERATIVO É FALSO
+#da pra colocar outras coisas aqui tbm, mas não sei oq tu vai colocar, então to nem ai 
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	interativo = false
+
+#TU SABE OQ O _PROCESS FAZ NÉ?
+func _process(delta: float) -> void:
+	#o if verifica se a tecla E ta sendo pressionada e se o interativo é verdadeiro, se sim
+	#ele executa o Dialogic, acho que da pra fazer o player parar de andar com isso aqui, mas não sei dizer
+	if Input.is_action_just_pressed("interact") and interativo == true:
+		Dialogic.start(dialogo)
--- Assets/Scripts/player_movement.gd
+++ Assets/Scripts/player_movement.gd
@@ -3,8 +3,6 @@ extends CharacterBody2D
 @export var movement_speed: float=500
 var character_direction: Vector2
 
-var inteerativo = true
-
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("move_left", "move_right")
 	character_direction.y = Input.get_axis("move_up", "move_down")
@@ -20,10 +18,3 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 	
 	move_and_slide()
-	#FUNÇÃO QUE CHAMA O TESTE DO DIALOGIC
-	dialogic()
-
-#TESTA DIALOGIC
-func dialogic():
-	if Input.is_action_just_pressed("interact") and inteerativo == true:
-		Dialogic.start("timeline")
