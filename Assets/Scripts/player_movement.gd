b998fddc7b6c8159e9897dacafbe3e501f5e2ebb
NamoeL19 <manumachadog@gmail.com>
2025-02-24 14:04:58 -0300

    player parada de andar no dialogo

--- Assets/Scripts/player_movement.gd
+++ Assets/Scripts/player_movement.gd
@@ -2,24 +2,27 @@ extends CharacterBody2D
 
 @export var movement_speed: float=500
 var character_direction: Vector2
+var parado
 
-func _physics_process(delta):
-	character_direction.x = Input.get_axis("move_left", "move_right")
-	character_direction.y = Input.get_axis("move_up", "move_down")
+func _physics_process(delta):	
+	parado = Dialogic.VAR.get("podeandar")
 	
-	#flip
-	if Input.is_action_pressed("move_left"):
-		$sprite.play("side")
-		$sprite.flip_h=true
-	elif Input.is_action_pressed("move_right"):
-		$sprite.play("side")
-		$sprite.flip_h=false
-	elif Input.is_action_pressed("move_up"):
-		$sprite.play("up")
-	elif Input.is_action_pressed("move_down"):
-		$sprite.play("down")
-	else:
-		$sprite.stop()
+	#flip e movimentação
+	if not parado:
+		character_direction.x = Input.get_axis("move_left", "move_right")
+		character_direction.y = Input.get_axis("move_up", "move_down")
+		if Input.is_action_pressed("move_left"):
+			$sprite.play("side")
+			$sprite.flip_h=true
+		elif Input.is_action_pressed("move_right"):
+			$sprite.play("side")
+			$sprite.flip_h=false
+		elif Input.is_action_pressed("move_up"):
+			$sprite.play("up")
+		elif Input.is_action_pressed("move_down"):
+			$sprite.play("down")
+		else:
+			$sprite.stop()
 	if character_direction:
 		velocity = character_direction * movement_speed
 		
