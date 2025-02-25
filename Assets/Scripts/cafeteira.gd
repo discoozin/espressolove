d77d53f49a39761a9662856026c6e7d4af4571d2
NamoeL19 <manumachadog@gmail.com>
2025-02-25 12:42:26 -0300

    ARRUMEI A CAGADA Q FIZ

--- Assets/Scripts/cafeteira.gd
+++ Assets/Scripts/cafeteira.gd
@@ -7,7 +7,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	interact=false
-func _process(delta):
+func _process(delta): #corrigir tamanho da cena do cafe
 	if interact==true:
 		if Input.is_action_just_pressed("interact"):
 			get_tree().change_scene_to_file("res://Assets/Scenes/cafecena.tscn")
