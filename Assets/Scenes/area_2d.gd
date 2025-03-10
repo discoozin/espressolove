26ff55de0d6ba04e189a9e47d4c05f0ecb36d0fc
discoozin <d1sc0nn3ctzz@gmail.com>
2025-03-10 00:56:03 -0300

    estoque

---
+++ Assets/Scenes/area_2d.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _ready():
+	if Input.is_action_just_pressed("move_down"):
+		print("penis")
---
+++ Assets/Scenes/salaestoque2.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var entrar = false
+func _on_body_entered(body: CharacterBody2D) -> void:
+	entrar=true
+
+func _on_body_exited(body: Node2D) -> void:
+	entrar=false
+
+func _process(delta):
+	if entrar==true:
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://Assets/Scenes/main.tscn")
